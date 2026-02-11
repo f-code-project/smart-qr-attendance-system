83ffd3e2b0bc92816451c6acc91db65d6f2d8e9e
hoangtuanqn <phamhoangtuanqn@gmail.com>
2026-02-12 00:28:30 +0700

    feat: setup https

---
+++ apps/web/vite.config.ts
@@ -0,0 +1,23 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+    mkcert(),
+  ],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src'),
+    },
+  },
+});
