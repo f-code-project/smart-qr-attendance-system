f642ffd3d8061ec72e1016a902cc9c333cc70708
hoangtuanqn <phamhoangtuanqn@gmail.com>
2026-02-13 21:15:55 +0700

    feat: sign access token and refresh token

--- apps/api/src/constants/enum.ts
+++ apps/api/src/constants/enum.ts
@@ -1,13 +1,13 @@
 // copy project cũ: https://github.com/f-code-project/fcode-web-system-challenge-3/blob/main/backend/src/constants/enums.ts
 
 export enum TokenType {
-  AccessToken,
-  RefreshToken,
-  ActivateAccount,
+  ACCESS = 'ACCESS',
+  REFRESH = 'REFRESH',
+  ACTIVATE = 'ACTIVATE',
 }
 
 export enum ExpiresInTokenType {
-  AccessToken = 30 * 24 * 60 * 60, // 30 ngày
-  RefreshToken = 30 * 24 * 60 * 60, // 30 ngày
-  ActivateAccount = 24 * 60 * 60, // 24 giờ
+  ACCESS = 5 * 60, // 5 phút
+  REFRESH = 30 * 24 * 60 * 60, // 30 ngày
+  ACTIVATE = 24 * 60 * 60, // 24 giờ
 }
