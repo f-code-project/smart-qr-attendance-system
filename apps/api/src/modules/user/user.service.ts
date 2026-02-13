3551ee4cea7b8024cd82be1d55741282b65fe8a3
hoangtuanqn <phamhoangtuanqn@gmail.com>
2026-02-13 22:02:51 +0700

    improve: login

--- apps/api/src/modules/user/user.service.ts
+++ apps/api/src/modules/user/user.service.ts
@@ -1,27 +1,34 @@
 import { redis } from 'bun';
 import { v7 as uuidV7 } from 'uuid';
-import { TokenType } from '../../constants/enum';
+import { ExpiresInTokenType, TokenType } from '../../constants/enum';
 import HashUtils from '../../lib/hash';
 import JwtUtils from '../../lib/jwt';
 import userRepository from './user.repository';
 
 class UserService {
   async login(email: string, password: string) {
-    const count = await this.checkCountErrrorPwd(email);
+    const count = await this.checkCountErrorPwd(email);
     if (count >= 5) throw new Error('Tài khoản của bạn tạm thời bị khóa do đăng nhập sai nhiều lần!');
 
     const user = await userRepository.getUserByEmail(email);
-    if (!user) throw new Error('Không tìm thấy tài khoản của bạn!');
-
-    const pwdHash = await HashUtils.verifyPassword(password, user.password);
-    if (!pwdHash) {
-      // add vô redis +=1 login sai mk
-      await this.inckCountErrrorPwd(email);
-      throw new Error('Mật khẩu của bạn không chính xác!');
+    if (!user) {
+      await this.incrCountErrorPwd(email); // vẫn incr tránh brute force
+      throw new Error('Email hoặc mật khẩu không chính xác!'); // luôn trả về tb chung chung :((((
     }
-    const { accessToken, refreshToken } = await this.signToken(email);
 
-    // sign access token + refresh token
+    const isCorrectPwd = await HashUtils.verifyPassword(password, user.password);
+    if (!isCorrectPwd) {
+      // add vô redis +=1 login sai mk
+      await this.incrCountErrorPwd(email);
+      throw new Error('Email hoặc mật khẩu không chính xác!'); // luôn trả về tb chung chung :((((
+    }
+    // login thành công thì xóa count login sai
+    await redis.del(`lock:${email}`);
+
+    const { accessToken, refreshToken, jti } = await this.signToken(email);
+
+    await redis.set(`RT:${user.id}:${jti}`, refreshToken, 'EX', ExpiresInTokenType.REFRESH);
+
     return {
       ...user,
       accessToken,
@@ -30,20 +37,24 @@ class UserService {
   }
 
   private async signToken(email: string) {
-    const accessToken = await JwtUtils.signToken(email, TokenType.ACCESS);
+    const { token: accessToken, jti } = await JwtUtils.signToken(email, TokenType.ACCESS);
     const refreshToken = uuidV7();
     return {
-      accessToken: accessToken.token,
+      accessToken,
       refreshToken,
+      jti,
     };
   }
-  private async checkCountErrrorPwd(email: string) {
-    const count = await redis.get(`lock:${email}`);
+  private async checkCountErrorPwd(email: string) {
+    const key = `lock:${email}`;
+    const count = await redis.get(key);
     return Number(count || 0);
   }
 
-  private async inckCountErrrorPwd(email: string) {
-    const count = await redis.incr(`lock:${email}`);
+  private async incrCountErrorPwd(email: string) {
+    const key = `lock:${email}`;
+    const count = await redis.incr(key);
+    if (count == 1) await redis.expire(key, 60 * 15); // lock trong 15p từ lần sau đầu tiên
     return count;
   }
 }
