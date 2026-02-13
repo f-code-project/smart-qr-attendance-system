2c76f3ab4b22a6e16a28feaba214d6f3226ad441
hoangtuanqn <phamhoangtuanqn@gmail.com>
2026-02-14 00:35:14 +0700

    feat: catch error

--- apps/api/src/middlewares/error-handler.middleware.ts
+++ apps/api/src/middlewares/error-handler.middleware.ts
@@ -1,12 +1,19 @@
 import { Context } from 'hono';
 import ApiResponse from '../common/api-response';
+import AppError from '../common/app-error';
 
 export const errorHandler = async (err: any, c: Context) => {
   console.error(`[Error Log]: ${err.stack || err.message}`);
   // lỗi của hệ thống nên mới có status (do mặc định đều phải ném ra lỗi ApiResonse.error)
-  if (err.status) {
-    return ApiResponse.error(c, err.message, err, err.status);
+  if (err instanceof AppError) {
+    return ApiResponse.error(c, err.message, err.errorCode, err.errors, err.statusCode);
   }
   // lỗi của hệ thống, do k có .StatusRequest
-  return ApiResponse.error(c, 'Đã có lỗi xảy ra bên phía máy chủ. Vui lòng liên hệ BTQ F-Code', err, 500);
+  return ApiResponse.error(
+    c,
+    'Đã có lỗi xảy ra bên phía máy chủ. Vui lòng liên hệ BTQ F-Code',
+    'INTERNAL_SERVER_ERROR',
+    null,
+    500,
+  );
 };
