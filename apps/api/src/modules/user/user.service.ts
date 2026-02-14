170dbecc4b16def239f6cc0e69b2c1ed000df1a0
hoangtuanqn <phamhoangtuanqn@gmail.com>
2026-02-15 03:51:45 +0700

    style: variable key lock constructor

--- apps/api/src/modules/user/user.service.ts
+++ apps/api/src/modules/user/user.service.ts
@@ -3,12 +3,16 @@ import { v7 as uuidV7 } from 'uuid';
 import AppError from '../../common/app-error';
 import { ErrorCode } from '../../constants/error-code';
 import { HttpStatusCode } from '../../constants/http-status';
-import { ExpiresInTokenType, TokenType } from '../../constants/token';
+import { TokenType } from '../../constants/token';
 import HashUtils from '../../lib/hash';
 import JwtUtils from '../../lib/jwt';
 import userRepository from './user.repository';
 
 class UserService {
+  private keyLock: string;
+  constructor() {
+    this.keyLock = 'lock:';
+  }
   async login(email: string, password: string) {
     const count = await this.checkCountErrorPwd(email);
     // if (count >= 5) throw new Error('Tài khoản của bạn tạm thời bị khóa do đăng nhập sai nhiều lần!');
@@ -25,7 +29,7 @@ class UserService {
     if (!user) {
       await this.incrCountErrorPwd(email); // vẫn incr tránh brute force
       throw new AppError(
-        'Email hoặc mật khẩu không chính xác',
+        'Email hoặc mật khẩu không chính xác!',
         ErrorCode.AUTH_INVALID_CREDENTIALS,
         null,
         HttpStatusCode.UNAUTHORIZED,
@@ -37,18 +41,18 @@ class UserService {
       // add vô redis +=1 login sai mk
       await this.incrCountErrorPwd(email);
       throw new AppError(
-        'Email hoặc mật khẩu không chính xác',
+        'Email hoặc mật khẩu không chính xác!',
         ErrorCode.AUTH_INVALID_CREDENTIALS,
         null,
         HttpStatusCode.UNAUTHORIZED,
       ); // luôn trả về tb chung chung :((((
     }
     // login thành công thì xóa count login sai
-    await redis.del(`lock:${email}`);
+    await this.removeErrorCountPwd(email);
 
     const { accessToken, refreshToken, jti } = await this.signToken(email);
 
-    await redis.set(`RT:${user.id}:${jti}`, refreshToken, 'EX', ExpiresInTokenType.REFRESH);
+    // await redis.set(`RT:${user.id}:${jti}`, refreshToken, 'EX', ExpiresInTokenType.REFRESH);
 
     return {
       // ...user,
@@ -68,16 +72,21 @@ class UserService {
     };
   }
   private async checkCountErrorPwd(email: string) {
-    const key = `lock:${email}`;
+    const key = `${this.keyLock}${email}`;
     const count = await redis.get(key);
     return Number(count || 0);
   }
 
   private async incrCountErrorPwd(email: string) {
-    const key = `lock:${email}`;
+    const key = `${this.keyLock}${email}`;
     const count = await redis.incr(key);
     if (count == 1) await redis.expire(key, 60 * 15); // lock trong 15p từ lần sau đầu tiên
     return count;
+  }
+
+  private async removeErrorCountPwd(email: string) {
+    const key = `${this.keyLock}${email}`;
+    await redis.del(key);
   }
 
   private async saveRefreshToken(userId: string, jti: string, refreshToken: string, expiresAt: Date) {
